a80952263ea916e75b520d9593845a277f5a28e2
lvliangshajizhi <799308867@qq.com>
2018-08-12 22:43:22 +0800

    2 Commit,完善json

--- src/main/java/com/tww/myproject/web/UserController.java
+++ src/main/java/com/tww/myproject/web/UserController.java
@@ -2,6 +2,7 @@ package com.tww.myproject.web;
 
 import com.tww.myproject.entity.UserInfo;
 import com.tww.myproject.service.UserInfoService;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,11 +19,18 @@ public class UserController {
     private UserInfoService userInfoService;
 
     @RequestMapping(value = "/getUserInfo",method= RequestMethod.GET)
-    private Map<String,UserInfo> getUser(){
+    private String getUser(){
         UserInfo userInfo=userInfoService.getUserInfo("ccc","789",false);
-        Map<String,UserInfo> userInfoMap=new HashMap<String,UserInfo>();
-        userInfoMap.put("user",userInfo);
-        return userInfoMap;
+        Map<String,Object> userInfoMap=new HashMap<String,Object>();
+        userInfoMap.put("MsgId","8888888");
+        userInfoMap.put("userId",userInfo.getUserId());
+        userInfoMap.put("Msgbody",userInfo);
+        userInfoMap.put("Msgtype","随机");
+        //java对象变成json对象
+        JSONObject jsonObject=JSONObject.fromObject(userInfoMap);
+        //json对象转换成json字符串
+        String jsonStr=jsonObject.toString();
+        return jsonStr;
     }
 
 }
